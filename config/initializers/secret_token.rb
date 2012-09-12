3217c10a1e2a767ff9fff9aea77a0100db1d8218
Ruby and Beer <rubyandbeer@gmail.com>
2012-09-12 00:15:21 +0000

    First commit. Create rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rubyandbeer::Application.config.secret_token = '4caed418ce736738147c97bff347171bef6d61109c30481844327dc6b190b759833090ce4b2f0e17724b4b880d6ce2dda67e072d003fe02352f4a91ff3721d58'
